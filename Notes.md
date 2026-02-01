75f0ad8abc7b70f5ac12a326031c007e1bfe2123
AKASHTS2003 <akash.t.shaju@gmail.com>
2026-02-01 13:07:23 +0530

    Initial commit - React portfolio setup

---
+++ Notes.md
@@ -0,0 +1,5 @@
+1.Beginner freindly project structure how to set up files
+    1.1.Src Folder is the main folder
+    in src folder mainly there are components,context,data,hooks,pages,utils(and in every folder there is a test folder)
+        1.1.2 components(form,ui)
+        1.1.3 pages(Home,Login,Signup,Settings)
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  const name = "Akash"
+  const year = 2026
+
+  function sayHello() {
+    alert("Hello Akash!")
+  }
+
+  return (
+    <div>
+      <h1>{name}'s Portfolio</h1>
+      <p>Year: {year}</p>
+
+      <button onClick={sayHello}>Click Me</button>
+    </div>
+  )
+}
+
+export default App
