ee5a2cb5a5580d74fccd1ee3dd579b2234b59000
AKASHTS2003 <akash.t.shaju@gmail.com>
2026-02-01 13:55:12 +0530

    just added alias in viteconfig and added some backup folder for future use

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   server:{
     port:8000
+  },
+  //--resolve configuration is primarily used to map import aliases
+  //./src can be used as @
+  resolve:{
+    alias:{
+      "@":path.resolve(__dirname,"./src")
+    }
   }
 })
